a0704df58ba8079bf95869d52651f3d3548376c3
Ikechi Michael <mykehell123@gmail.com>
2018-02-08 15:04:16 +0100

    feat: add console.info

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ module.exports = (name) => {
     return {
         log: (message, ...args) => console.log(`${name}:`, colors.FgCyan, message, ...args, colors.Reset),
         warn: (message, ...args) => console.warn(`${name} (warn):`, colors.FgYellow, message, ...args, colors.Reset),
-        error: (message, ...args) => console.error(`${name} (error):`, colors.FgRed, message, ...args, colors.Reset)
+        error: (message, ...args) => console.error(`${name} (error):`, colors.FgRed, message, ...args, colors.Reset),
+        info: (message, ...args) => console.info(`${name} (error):`, colors.FgBlue, message, ...args, colors.Reset)
     }
 }
